cd4b9a97747f476e2acf688a1c897e35b4072173
diego <2019300@collegemv.qc.ca>
2022-07-19 21:43:14 -0400

    store set up part 2

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const btn = document.querySelector('.nav-btn');
 const links = document.querySelector('.links');
 
 btn.addEventListener('click', function open(){
-
     links.classList.toggle ("show-links");
 })
